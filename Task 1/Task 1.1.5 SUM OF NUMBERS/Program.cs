3fd51da111d26ff0499f257003a758a47147ca40
Vadim <vedeneevvd@gmail.com>
2022-01-09 17:52:35 +0400

    Add Task 1.1.5
    Add and Complete Task 1.1.5

---
+++ Task 1/Task 1.1.5 SUM OF NUMBERS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._1._5_SUM_OF_NUMBERS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sum = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    Sum += i;
+            }
+            Console.WriteLine($"Сумма всех кратных чисел от 1 до 1000: {Sum}");
+            Console.ReadLine();
+        }
+    }
+}
