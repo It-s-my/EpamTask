8f70f49b18778780cc58e9c59533259d44f272a6
Vadim <vedeneevvd@gmail.com>
2022-01-09 18:21:02 +0400

    Add Task 1.1.6
    Add Task

---
+++ Task 1/Task 1.1.6 FONT ADJUSTMENT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._1._6_FONT_ADJUSTMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bold = "Bold";
+            string italic = "Italic";
+            string underline = "Underline";
+        }
+    }
+}
