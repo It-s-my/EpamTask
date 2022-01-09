4460aaff9267cc0ea5e8d2ea2e627679168f5670
Vadim <vedeneevvd@gmail.com>
2022-01-09 16:13:32 +0400

    Edit Task 1.1.1
    Task 1.1.1 Complete

--- Task 1/Task 1.1.1/Task_1.1.1_Rectangle/Task_1.1.1_Rectangle/Program.cs
+++ Task 1/Task 1.1.1/Task_1.1.1_Rectangle/Task_1.1.1_Rectangle/Program.cs
@@ -10,6 +10,36 @@ namespace Task_1._1._1_Rectangle
     {
         static void Main(string[] args)
         {
+            int S;
+            Console.WriteLine("Введите значение а:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a < 0 || a == 0)
+            {
+                Console.WriteLine("Ошибка.Попробуйте Ввести другое значение а");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+                Console.WriteLine("Введите значение b:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                if (b < 0 || b == 0){
+                Console.WriteLine("Ошибка.Попробуйте Ввести другое значение b");
+                b = Convert.ToInt32(Console.ReadLine());
+                 }
+            if (a < 0 || a == 0)
+            {
+                Console.WriteLine("Неверно введены данные");
+                Console.ReadKey();
+            }
+            else if (b < 0 || b == 0)
+            {
+                Console.WriteLine("Неверно введены данные");
+                Console.ReadKey();
+            }
+            else
+            {
+                S = a * b;
+                Console.WriteLine($"Сторона а: {a} Сторона b: {b} Площадь прямоугольника S: {S}");
+                Console.ReadKey();
+            }
         }
     }
 }
