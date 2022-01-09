691f10512847945146731b1655396ecf446c0062
Vadim <vedeneevvd@gmail.com>
2022-01-09 16:41:59 +0400

    Complete Task 1.1.2
    Add Task 1.1.2, Edit task

---
+++ Task 1/Task 1.1.2 TRIANGLE/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._1._2_TRIANGLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i <= n; i++)
+            {
+                Console.WriteLine(new string('*', i + 1));
+            }
+            Console.ReadKey();
+        }
+    }
+}
