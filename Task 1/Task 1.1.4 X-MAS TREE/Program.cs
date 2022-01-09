eaa2658d8172b8aebafb6f1bf1ba8df7dd67af0d
Vadim <vedeneevvd@gmail.com>
2022-01-09 17:41:11 +0400

    Add Task 1.1.4
    Complete Task 1.1.4

---
+++ Task 1/Task 1.1.4 X-MAS TREE/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._1._4_X_MAS_TREE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < (n+i)/2 ; j++)
+                {
+                    int N_Space = n - j - 1;
+                    int N_Star = 1 + j * 2 ;
+                    string Space = new string(' ', N_Space);
+                    Console.WriteLine(Space + new string('*', N_Star));
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
