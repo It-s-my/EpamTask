b589585529eb4dabb1dfb36299e3cb68a97c78c4
Vadim <vedeneevvd@gmail.com>
2022-01-09 16:59:08 +0400

    Add Task 1.1.3
    Add and Complete Task 1.1.3

---
+++ Task 1/Task 1.1.3. ANOTHER TRIANGLE/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._1._3.ANOTHER_TRIANGLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                int N_Space = n - i - 1;
+                int N_Star = 1 + i * 2;
+                string Space = new string(' ', N_Space);
+                Console.WriteLine(Space + new string('*', N_Star) + Space);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
