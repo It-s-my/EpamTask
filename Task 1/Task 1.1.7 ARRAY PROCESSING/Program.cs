7a0ad196b11a84318d58d383fabc336ae6f838fe
Vadim <vedeneevvd@gmail.com>
2022-01-09 18:39:53 +0400

    Add Task 1.1.7
    Began to do the task.Add random array generation.

---
+++ Task 1/Task 1.1.7 ARRAY PROCESSING/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._1._7_ARRAY_PROCESSING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[5];
+            Random(arr);
+            
+            
+
+            Console.ReadKey();
+        }
+        private static int[] Random(int[] arr)
+        {
+            var random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(-100, 100);
+            }
+
+            return arr;
+        }
+    }
+}
